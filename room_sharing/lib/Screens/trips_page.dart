cc7d1fc85a6daca7dc9a189c33a4de113f72d6c5
Sumith Damodaran <sumithpd@gmail.com>
2021-11-18 18:34:44 +0000

    Trips page, with trips list and tile

--- room_sharing/lib/Screens/trips_page.dart
+++ room_sharing/lib/Screens/trips_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:room_sharing/Views/grid_widgets.dart';
 
 class TripsPage extends StatefulWidget {
   const TripsPage({Key? key}) : super(key: key);
@@ -12,6 +13,61 @@ class TripsPage extends StatefulWidget {
 class _TripsPageState extends State<TripsPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('trips Page'));
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(25, 25, 25, 0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Upcoming Trips',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0, bottom: 25.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 15.0),
+                      child: Container(
+                          width: MediaQuery.of(context).size.width /2.5,
+                          child: TripGridTile()),
+                    );
+                  },
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                ),
+              ),
+            ),
+            Text(
+              'Previous Trips',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15.0, bottom: 25.0),
+              child: Container(
+                height: MediaQuery.of(context).size.height / 3,
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 15.0),
+                      child: Container(
+                          width: MediaQuery.of(context).size.width /2.5,
+                          child: TripGridTile()),
+                    );
+                  },
+                  itemCount: 2,
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- room_sharing/lib/Views/grid_widgets.dart
+++ room_sharing/lib/Views/grid_widgets.dart
@@ -41,11 +41,74 @@ class _PostingGridTileState extends State<PostingGridTile> {
             fontSize: 16,
           ),
         ),
-        Text('\$120 / night', style: TextStyle(
-          fontSize: 15,
-        ),),
+        Text(
+          '\$120 / night',
+          style: TextStyle(
+            fontSize: 15,
+          ),
+        ),
         StarRating(
             editable: false, initialRating: 3.5, ratingSize: RatingSize.medium),
+      ],
+    );
+  }
+}
+
+class TripGridTile extends StatefulWidget {
+  const TripGridTile({Key? key}) : super(key: key);
+
+  @override
+  _TripGridTileState createState() => _TripGridTileState();
+}
+
+class _TripGridTileState extends State<TripGridTile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        AspectRatio(
+          aspectRatio: 3 / 2,
+          child: Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/images/apartment.jpg'),
+              fit: BoxFit.fill,
+            )),
+          ),
+        ),
+        AutoSizeText(
+          'Apartment  - London tower bridge',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 17,
+          ),
+        ),
+        AutoSizeText(
+          'London, UK',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        Text(
+          '\$120 / night',
+          style: TextStyle(
+            fontSize: 15,
+          ),
+        ),
+        Text(
+          '25 Dec 2021 - ',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          ' 05 Jan 2022',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ],
     );
   }
